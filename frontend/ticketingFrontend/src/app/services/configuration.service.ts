57380c202b3154b0d077140ee879808c7b5fad3c
ThaminduMiuranda <thaminduthilina@gmail.com>
2024-12-08 12:22:34 +0530

    feat: Created models and services for configuration

---
+++ frontend/ticketingFrontend/src/app/services/configuration.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Configuration } from '../models/configuration.model';
+// import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ConfigurationService {
+  private apiUrl = `http://localhost:8080/api/configuration`;
+
+  constructor(private http: HttpClient) {}
+
+  saveConfiguration(config: Configuration): Observable<any> {
+    return this.http.post(this.apiUrl, config,{ responseType: 'text' });
+  }
+
+  getConfiguration(): Observable<Configuration> {
+    return this.http.get<Configuration>(this.apiUrl);
+  }
+}
